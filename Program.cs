f310375b409400244ec6915e53236183439c1631
tplindqvist <88140163+tplindqvist@users.noreply.github.com>
2026-09-11 09:39:28 +0200

    Added simple lines..

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git!");
+        Console.WriteLine(DoSomething("Tobias"));
+    }
+
+    static string DoSomething(string name)
+    {
+        return $"Hello{name}";
     }
 }
